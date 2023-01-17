c86d4f5b349483b20f1f44511167a18d4e441f81
Stijn van der Made <stijnmade@gmail.com>
2023-01-17 14:12:46 +0100

    add setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Make script an executable
+sudo chmod +x ./access_point_installer.sh
+sudo chmod +x ./captive_portal_installer.sh
+sudo chmod +x ./firewall_installer.sh
+
+# Run the install scripts
+sudo ./access_point_installer.sh
+sudo ./captive_portal_installer.sh
+sudo ./firewall_installer.sh
